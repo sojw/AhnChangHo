05de80cb3d611ec37f3fa0a3b53d0c48d255bf95
GunPyo Park <naver.blog@navercorp.com>
2016-09-11 02:28:17 +0900

    first commit

---
+++ src/test/java/com/sojw/ahnchangho/DividendGrabTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.sojw.ahnchangho;
+
+import org.junit.Test;
+
+/**
+ * @author Naver
+ *
+ */
+public class DividendGrabTest {
+
+	/**
+	 * Test method for {@link com.sojw.ahnchangho.DividendGrab#test()}.
+	 */
+	@Test
+	public void testTest() throws Exception {
+		DividendGrab sut = new DividendGrab();
+		//sut.grab();
+	}
+
+}
